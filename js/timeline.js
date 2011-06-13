6c922998964083c23899f6a13f9cdfef9b164b02
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-06-12 22:24:36 -0600

    Playing around with charts.

--- js/timeline.js
+++ js/timeline.js
@@ -1,35 +1,42 @@
 $( function() {
-    
-    var r, container, width, height, data;
-    container = $("#timeline-container")[0];
-    
-    width = $(container).width();
-    height = $(container).height();
-    
-    data = populateData();
+	
+	//	Draws main timeline 
+	function drawTimeline()
+	{
+		var r, container, width, height, data;
+		container = $("#timeline-container")[0];
 
-    r = Raphael(container);
-    r.g.barchart(0, 20, width, height, [data], {colors:["#666666"], gutter:"10%"}).hover(fadeIn, fadeOut);
+		width = $(container).width();
+		height = $(container).height();
 
-    function fadeIn ()
-    {
-        this.flag = r.g.popup(this.bar.x, this.bar.y, (this.bar.value || "0") + " records").insertBefore(this);
-    }
-    
-    function fadeOut ()
-    {
-        this.flag.animate({opacity: 0}, 100, function () {this.remove();});
-    }
-    
-     
-    function populateData()
-    {
-        var result = [];
-        var len = 50+Math.random()*100;
-        for(i = 0; i < len; i += 1)
-        {
-            result.push(Math.round(Math.random()*(len-i)));
-        }
-        return result;
-    }
+		data = populateData();
+
+		r = Raphael(container);
+		r.g.barchart(0, 20, width, height, [data], {colors:["#555555"], gutter:"10%"}).hover(fadeIn, fadeOut);
+
+		function fadeIn ()
+		{
+			this.flag = r.g.popup(this.bar.x, this.bar.y, (this.bar.value || "0") + " records").insertBefore(this);
+		}
+
+		function fadeOut ()
+		{
+			this.flag.animate({opacity: 0}, 100, function () {this.remove();});
+		}
+
+
+		function populateData()
+		{
+			var result = [];
+			var len = 50+Math.random()*100;
+			for(i = 0; i < len; i += 1)
+			{
+				result.push(Math.round(Math.random()*(len-i)));
+			}
+			return result;
+		}
+	}
+	
+	
+	drawTimeline();
 });
