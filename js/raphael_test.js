6775d675293938b6527b15f436ed40ebf1fdbbb9
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-07-20 16:54:40 -0600

    Updated histogram to work with populated data.

--- js/raphael_test.js
+++ js/raphael_test.js
@@ -3,24 +3,43 @@
  */
 $(function ()
 {
-	var canvas, drawing;
 	
-	canvas = $("#canvas")[0];
-	
-	drawing = Raphael(canvas, $(canvas).width(), $(canvas).height());
-	
-	var i, len = 100, min=0, max=300, percent, barW, barH, barX, barY, barPadding=2, bar;
-	barW = $(canvas).width() / len;
-	
-	for(i = 0; i < len; i += 1)
+	/**
+	 * Draws timeline histogram.
+	 * @param canvas Div histogram container
+	 * @param data Data container [12, 22, 30]
+	 */
+	function drawHistogram(canvas, data)
 	{
-		percent = (Math.random()*(max-i) / max) * 100;
-		barH = Math.round(percent * max / 100);
-		barX = Math.round(i*(barW+barPadding));
-		barY = Math.round( $(canvas).height() - barH)
+		var histogram, i, len = data.length, max=25, maxHeight=$(canvas).height(), percent, barW, barH, barX, barY, barPadding=2, bar;
 		
-		bar = drawing.rect(barX, barY, barW, barH);
-		bar.attr({fill: "#000", "stroke-width":0});
+		histogram = Raphael(canvas, $(canvas).width(), $(canvas).height());
+		
+		barW = ($(canvas).width()-(barPadding*len)) / len;
+		
+		for(i = 0; i < len; i += 1)
+		{
+			percent = (data[i] / max) * 100;
+			barH = Math.round(percent * maxHeight / 100);
+			barX = Math.round(i*(barW+barPadding));
+			barY = Math.round( $(canvas).height() - barH)
+			
+			bar = histogram.rect(barX, barY, barW, barH);
+			bar.attr({fill: "#000", "stroke-width":0});
+		}
 	}
 	
+	
+	function populateData()
+	{
+		var result = [], i, len;
+		len = 25;//Math.random()*200;
+			
+		for(i = 0; i <= len; i += 1)
+			result.push(Math.round(Math.random()*(len-i)));
+		
+		return result;
+	}
+	
+	drawHistogram($("#canvas")[0], populateData());
 });
