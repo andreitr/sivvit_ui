9431645324f6a4a4293c0034f48aa642120307fb
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-09-29 22:12:02 -0600

    Latest version of the embed test.

--- embed/js/embed.js
+++ embed/js/embed.js
@@ -5,7 +5,7 @@ $(function()
 	// Add embed .swf
 	flashvars = {source:"json/event.json"};
 
-	swfobject.embedSWF("swf/Application.swf","flash", "100%", "600", "10.0.0","swf/playerProductInstall.swf", flashvars, {wmode:"opaque"}, {id:"sivvitEmbed", name:"sivvitEmbed"});
+	swfobject.embedSWF("swf/Application.swf","flash", "100%", "420", "10.0.0","swf/playerProductInstall.swf", flashvars, {wmode:"opaque"}, {id:"sivvitEmbed", name:"sivvitEmbed"});
 	
 	
 });
