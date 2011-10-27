ed8eb002ddf805bfb50f79adf16a5d1d22813fef
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-10-27 15:15:17 -0600

    js/

--- js/map.js
+++ js/map.js
@@ -1,13 +1,4 @@
 $( function()
 {
-	var latlng = new google.maps.LatLng(-34.397, 150.644);
-	
-    var myOptions = {
-      zoom: 9,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: true
-    };
-    
-    var map = new google.maps.Map(container = $("#mapCanvas")[0], myOptions);
+
 });
