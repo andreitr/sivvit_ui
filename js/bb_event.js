ab7bb4d86c50158ef40432f656e6bfbf91da9aff
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-10-27 20:07:59 -0600

    Implemented histogram ui and model.

--- js/bb_event.js
+++ js/bb_event.js
@@ -5,7 +5,8 @@ $(document).ready(function(jQuery)
 		defaults:
 		{
 			startDate:"",
-			endDate:""
+			endDate:"",
+			histogram:[]
 		}
 	});
 	
@@ -21,11 +22,49 @@ $(document).ready(function(jQuery)
 		},
 		
 		render : function() {
-			$(this.el).html(this.model.get("startDate"));
-			$(this.el).append(this.model.get("endDate"));
 			
-			console.log(this.model.startDate);			
+			$(this.el).html(this.model.get("startDate"));
+			this.drawHistogram();
 		},
+		
+		
+		drawHistogram: function ()
+		{
+			var histogram, i, len, maxVal, minVal, maxHeight, percent, barW, barH, barX, barY, barXPadding;
+			len = this.model.get("histogram").length;
+		
+			maxVal = 6;
+			minVal = 2;
+			maxHeight = $(this.el).height() - 20;
+			barXPadding = 1;
+			histogram = Raphael($(this.el)[0], $(this.el).width(), $(this.el).height());
+			barW = ($(this.el).width() - (barXPadding * len)) / len;
+
+			for( i = 0; i < len; i += 1) {
+				percent = (this.model.get("histogram")[i].count / maxVal) * 100;
+				barH = Math.round(percent * maxHeight / 100);
+				barX = Math.round(i * (barW + barXPadding));
+				barY = Math.round($(this.el).height() - barH)
+
+				var bar = histogram.rect(barX, barY, barW, barH).attr( {gradient:"90-#333333-#555555", "stroke-width":0})
+				
+			
+				bar.mouseover(function() {
+					this.attr({
+						fill : "#007AA2",
+						cursor : "pointer"
+					});
+				});
+
+				bar.mouseout(function() {
+					this.attr( {gradient:"90-#333333-#555555", "stroke-width":0});
+				});
+
+
+			}
+
+		
+		}
 	});
 	
 	
@@ -67,11 +106,9 @@ $(document).ready(function(jQuery)
 	var histModel = new HistogramModel();
 	var histView = new HistogramView({model:histModel});
 
-	
 	$.getJSON("embed/json/event.json", {}, function(data) {
 
 		mapModel.set({location:data.location});
-		histModel.set({startDate:data.startDate, endDate:data.endDate});		
-
-	})
+		histModel.set({startDate:data.startDate, endDate:data.endDate, histogram:data.histogram.global});		
+	});
 });
