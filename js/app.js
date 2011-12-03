365d38f997f83e4aeae782fdcf90bf755bd01165
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-02 19:55:09 -0700

    Removed old flash embed file.

--- js/app.js
+++ js/app.js
@@ -7,6 +7,6 @@
 	$LAB.script("js/libs/jquery/jquery-ui-1.8.16.custom.min.js").wait();
 	
 	$LAB.script("js/event.js").wait(function(){
-		SIVVIT.Event.init("embed/json/event.json");
+		SIVVIT.Event.init("event.json");
 	});
 })();
