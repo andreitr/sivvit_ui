3addc2e3c5221d1b31f945bc9a4e32d3f672cc9e
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-05 21:43:32 -0700

    Started building out account interface. #25.

--- js/account_main.js
+++ js/account_main.js
@@ -11,6 +11,12 @@ if( typeof (SIVVIT) == 'undefined') {
 		init : function(json) {
 			var self = this;
 			
+			$("#user-avatar").append("<img src='http://a0.twimg.com/profile_images/1169898172/01_normal.jpg' width='48' height='48'>");
+			$("#user-title").append("andreitr (Andrei Taraschuk)");
+		
+			$("#user-meta").html("<span class='icon-location'></span>Denver, Co");
+			$("#user-meta").append("&nbsp<span class='icon-user'></span>membed since January 24, 1981");
+			
 			$("#event-application").show();
 		}
 	};
