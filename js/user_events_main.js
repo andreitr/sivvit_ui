6e9ff6912f973694bde7af8b8eb4a21f368ea634
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-12 11:34:34 -0700

    Added pause / play control buttons.

--- js/user_events_main.js
+++ js/user_events_main.js
@@ -265,7 +265,7 @@ if( typeof (SIVVIT) == 'undefined') {
 				// Initiate button clicks if a user is logged in and modify
 				// content template (add hover buttons and check box)
 				if(this.edit) {
-					itm.html.find("#content").prepend("<span class=\"item-edit\"><span class=\"icon-delete\" id=\"del-itm\"></span><span class=\"icon-check\" id=\"apr-itm\"></span><span class=\"icon-cog\" id=\"edit-itm\"></span><div id=\"pending-flag\"></div></span>");
+					itm.html.find("#content").prepend("<span class=\"item-edit\"><span class=\"icon-delete\" id=\"del-itm\"></span><span class=\"icon-play\" id=\"apr-itm\"></span><span class=\"icon-cog\" id=\"edit-itm\"></span><div id=\"pending-flag\"></div></span>");
 					itm.html.find("#content").prepend("<div id=\"checkbox\"><input type=\"checkbox\" id=\"itm-check\"/></div>");
 
 					itm.html.find("#del-itm").hide();
@@ -294,7 +294,11 @@ if( typeof (SIVVIT) == 'undefined') {
 							case "del-itm":
 								self.deleteItem(itm);
 								break;
-
+							
+							case "edit-itm":
+								//Edit item
+								break;
+							
 							default:
 								if(itm.html.find("#itm-check").length > 0) {
 									checked = itm.html.find("#itm-check").is(':checked');
@@ -334,9 +338,14 @@ if( typeof (SIVVIT) == 'undefined') {
 		
 		showHidePending : function(itm) {
 			if(itm.model.get("status") === 1) {
+				itm.html.find("#apr-itm").toggleClass("icon-play", false);
+				itm.html.find("#apr-itm").toggleClass("icon-pause", true);
 				itm.html.find("#pending-flag").toggleClass("pending-notice", false);
 				itm.html.find("#pending-flag").toggleClass("active-notice", true);
 			} else {
+				itm.html.find("#apr-itm").toggleClass("icon-play", true);
+				itm.html.find("#apr-itm").toggleClass("icon-pause", false);
+				
 				itm.html.find("#pending-flag").toggleClass("pending-notice", true);
 				itm.html.find("#pending-flag").toggleClass("active-notice", false);
 			}
