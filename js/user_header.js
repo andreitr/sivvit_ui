db16bf9f637cb35de14f5b81c68d0b4ebd82743f
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-13 15:53:41 -0700

    Implemented user header.

--- js/user_header.js
+++ js/user_header.js
@@ -19,11 +19,11 @@ if( typeof (SIVVIT) == 'undefined') {
 			this.model.bind("change", function() {
 
 				// Move all this jazz into a separate view
-				$("#user-avatar").append("<img src='http://a0.twimg.com/profile_images/1169898172/01_normal.jpg' width='48' height='48'>");
+				$("#user-avatar").append("<img src='"+this.model.get("avatar")+"' width='48' height='48'>");
 				$("#user-title").append(this.model.get("user_name")+" ("+this.model.get("real_name")+")");
 
 				$("#user-meta").html("<span class='icon-location'></span>"+this.model.get("location").name);
-				$("#user-meta").append("&nbsp<span class='icon-user'></span>membed since January 24, 1981");
+				$("#user-meta").append("&nbsp<span class='icon-user'></span>membed since "+new Date(this.model.get("joined")).toDateString());
 
 			}, this);
 		}
@@ -38,6 +38,7 @@ if( typeof (SIVVIT) == 'undefined') {
 			real_name : null,
 			user_name : null,
 			premium : 0,
+			joined:null,
 			location : {
 				name : "Null Island",
 				lon : 0,
