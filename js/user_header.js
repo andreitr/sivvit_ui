5b9f654b2c17c9536cce5dfe7d5fdb07c977220c
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-13 10:51:31 -0700

    Implementing user-header view.

--- js/user_header.js
+++ js/user_header.js
@@ -17,14 +17,12 @@ if( typeof (SIVVIT) == 'undefined') {
 			this.model.fetch();
 
 			this.model.bind("change", function() {
-				
-				console.log("loaded");
 
 				// Move all this jazz into a separate view
 				$("#user-avatar").append("<img src='http://a0.twimg.com/profile_images/1169898172/01_normal.jpg' width='48' height='48'>");
-				$("#user-title").append("andreitr (Andrei Taraschuk)");
+				$("#user-title").append(this.model.get("user_name")+" ("+this.model.get("real_name")+")");
 
-				$("#user-meta").html("<span class='icon-location'></span>Denver, Co");
+				$("#user-meta").html("<span class='icon-location'></span>"+this.model.get("location").name);
 				$("#user-meta").append("&nbsp<span class='icon-user'></span>membed since January 24, 1981");
 
 			}, this);
@@ -32,6 +30,20 @@ if( typeof (SIVVIT) == 'undefined') {
 	}
 
 	// Main model responsible for loading and mainaining data
-	SIVVIT.Model = Backbone.Model.extend({});
+	SIVVIT.Model = Backbone.Model.extend({
+
+		defaults : {
+
+			id : 0,
+			real_name : null,
+			user_name : null,
+			premium : 0,
+			location : {
+				name : "Null Island",
+				lon : 0,
+				lat : 0
+			}
+		}
+	});
 
 })($, SIVVIT);
