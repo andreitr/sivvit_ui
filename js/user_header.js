b9862eee46723b58f38bcc9e97ecdce21d529c53
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-13 16:49:09 -0700

    Updated ui and notification messages.

--- js/user_header.js
+++ js/user_header.js
@@ -9,21 +9,20 @@ if( typeof (SIVVIT) == 'undefined') {
 
 		// Initiates the application and loads the main data.
 		init : function(json) {
-			var self = this;
 
-			this.model = new SIVVIT.Model();
+			this.model = new SIVVIT.Model({url:json});
 
 			this.model.url = json;
 			this.model.fetch();
 
 			this.model.bind("change", function() {
+				
+				// Display all necessary data
+				$("#user-avatar").append("<img src='" + this.model.get("avatar") + "' width='48' height='48'>");
+				$("#user-title").append(this.model.get("user_name") + " (" + this.model.get("real_name") + ")");
 
-				// Move all this jazz into a separate view
-				$("#user-avatar").append("<img src='"+this.model.get("avatar")+"' width='48' height='48'>");
-				$("#user-title").append(this.model.get("user_name")+" ("+this.model.get("real_name")+")");
-
-				$("#user-meta").html("<span class='icon-location'></span>"+this.model.get("location").name);
-				$("#user-meta").append("&nbsp<span class='icon-user'></span>membed since "+new Date(this.model.get("joined")).toDateString());
+				$("#user-meta").html("<span class='icon-location'></span>" + this.model.get("location").name);
+				$("#user-meta").append("&nbsp<span class='icon-user'></span>membed since " + new Date(this.model.get("joined")).toDateString());
 
 			}, this);
 		}
@@ -38,7 +37,7 @@ if( typeof (SIVVIT) == 'undefined') {
 			real_name : null,
 			user_name : null,
 			premium : 0,
-			joined:null,
+			joined : null,
 			location : {
 				name : "Null Island",
 				lon : 0,
