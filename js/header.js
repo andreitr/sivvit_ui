b966feeb72d882cf0e717f28b07d82524324c916
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2011-12-21 16:18:36 -0700

    Generic navigation header.

---
+++ js/header.js
@@ -0,0 +1,13 @@
+(function() {
+
+	$("#new-event").fancybox({
+		'width' : 860,
+		'height' : 430,
+		'autoScale' : true,
+		'scrolling' : false,
+		'transitionIn' : 'fade',
+		'transitionOut' : 'fade',
+		'type' : 'iframe'
+	});
+
+})();
