e541ecc900b098f711c2fb66231c5af426057bbc
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2012-05-31 22:25:43 -0600

    Gruntfile.

---
+++ grunt.js
@@ -0,0 +1,58 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'YOUR_NAME; Licensed MIT */'
+    },
+    lint: {
+      files: ['grunt.js', 'js/app/*.js', 'js/app/**/*.js']
+    },
+    concat: {
+      // Event
+      dist: {
+        src: ['<banner:meta.banner>', '<file_strip_banner:js/app/globals.js>','<file_strip_banner:js/header.js>', '<file_strip_banner:js/event.js>'],
+        dest: 'js/app/GRUNT_TEST.js'
+      }
+    },
+    min: {
+      dist: {
+        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
+        dest: 'dist/FILE_NAME.min.js'
+      }
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: true
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit concat min');
+
+};
