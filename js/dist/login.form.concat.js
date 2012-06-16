b934b8acb0f012a6a4f990231041881218424e87
Andrei Taraschuk <andrei.taraschuk@gmail.com>
2012-06-16 15:51:34 -0600

    Form refactoring.

---
+++ js/dist/login.form.concat.js
@@ -0,0 +1,68 @@
+// JSLint variable definition and formatting
+/*global SIVVIT:true  */
+/*jslint white:true */
+
+if ( typeof (SIVVIT) === 'undefined') {
+    SIVVIT = {};
+}
+
+SIVVIT.Settings = {
+    host : 'http://sivvit.com'
+};
+
+// JSLint variable definition
+/*global jQuery:false, SIVVIT:true, $:false, Backbone:false, confirm:false */
+/*jslint white:true devel:true passfail:false sloppy:true*/
+
+( function(jQuery) {
+
+        SIVVIT.Login = {
+
+            // Initiates the application and loads the main data.
+            init : function() {
+
+                var self = this;
+
+                $('#js-app-form').submit(function() {
+
+                    $('#js-app-form').validate();
+                    if ($('#js-app-form').valid()) {
+                        $('.btn').toggleClass('disabled', true);
+                        $('.btn').html("Processing <span class='loader'></span>");
+                    }
+                    return false;
+                });
+
+                // Initiate form validation
+                $('#js-app-form').validate({
+
+                    // Don't place default label
+                    success : function(label) {
+                    },
+
+                    // Don't place default error message
+                    errorPlacement : function(error, element) {
+                    },
+
+                    highlight : function(element, errorClass) {
+                        $(element).parent().toggleClass('error', true);
+                    },
+
+                    unhighlight : function(element, errorClass) {
+                        $(element).parent().toggleClass('error', false);
+                    },
+
+                    rules : {
+                        email : {
+                            required : true,
+                            email : true
+                        },
+                        password : {
+                            required : true,
+                            minlength : 5
+                        }
+                    }
+                });
+            }
+        };
+    }());
